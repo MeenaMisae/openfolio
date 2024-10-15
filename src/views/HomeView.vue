3f35917486085e80599f268aa1296f1f04d73b83
MeenaMisae <meenahiwatashi@gmail.com>
2024-10-14 22:06:27 -0300

    fix: altera caminho do plano de fundo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
 <template>
   <div class="flex justify-between flex-col h-[70vh]">
     <div class="flex justify-center items-center h-[60%] w-full flex-col relative">
-      <img src="/images/background-blurs.png" alt="" class="absolute w-[100%] top-2 opacity-65 lg:hidden">
+      <img src="@/assets/images/background-blurs.png" alt="" class="absolute w-[100%] top-2 opacity-65 lg:hidden">
       <div class="w-[85%] flex flex-col space-y-2">
         <h1 class="text-[#E5E9F0] text-lg">Olá. Meu nome é</h1>
         <span class="text-5xl text-[#E5E9F0]">Meena Hiwatashi</span>
