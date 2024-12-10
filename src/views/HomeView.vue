9e27b9f3580b5405a3ecdd1a542c73b709acff70
MeenaMisae <meenahiwatashi@gmail.com>
2024-12-10 10:52:45 -0300

    fix: retira animacao de particulas da home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,48 +21,6 @@ onMounted(() => {
       slideShadows: false,
     },
   });
-  const canvas = document.querySelector('canvas');
-  if (canvas) {
-    const ctx = canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    function createParticle() {
-      return {
-        x: Math.random() * canvas.width,
-        y: Math.random() * canvas.height,
-        size: Math.random() * 1 + 0.001,
-        dx: (Math.random() - 0.5) * 1,
-        dy: (Math.random() - 0.5) * 1,
-        opacity: Math.random() * 0.5 + 0.5,
-      };
-    }
-    let particles = Array.from({ length: 40 }, createParticle);
-    function animate() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      particles = particles.filter(p => {
-        p.x += p.dx;
-        p.y += p.dy;
-
-        if (p.x < 0 || p.x > canvas.width || p.y < 0 || p.y > canvas.height) {
-          return false;
-        }
-
-        ctx.beginPath();
-        ctx.arc(p.x, p.y, p.size, 0, Math.PI * 2);
-        ctx.fillStyle = `rgba(255, 255, 255, ${p.opacity})`;
-        ctx.fill();
-        return true;
-      });
-      while (particles.length < 40) {
-        particles.push(createParticle());
-      }
-      requestAnimationFrame(animate);
-    }
-
-    animate();
-  } else {
-    console.error("Canvas element not found.");
-  }
 })
 </script>
 
