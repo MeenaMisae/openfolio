55894dc07d5263789e42c637adef972e35318c56
MeenaMisae <meenahiwatashi@gmail.com>
2024-12-10 10:53:15 -0300

    wip: construcao da pagina de projetos

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,49 +1,69 @@
 <script setup>
 const technologies = ['HTML', 'CSS', 'Vue', 'Laravel', 'Livewire'];
 const projects = {
-    '1': { 'title': '_wire-cotton', 'description': 'Painel de administração de produtos em um e-commerce. Permite o gerenciamento eficiente de itens, facilitando a experiência do usuário na gestão de um catálogo online.', 'src': '/images/projects/wire-cotton.avif', 'finished': true },
-    '2': { 'title': '_wire-cotton-api', 'description': 'API para o projeto Wire Cotton, fornecendo funcionalidades e dados para o painel de administração e garantindo a integridade e eficiência das operações de e-commerce.', 'src': '/images/projects/wire-cotton.avif', 'finished': true },
-    '3': { 'title': '_meenu', 'description': 'Projeto voltado para o setor alimentício e permite o gerenciamento de produtos, incluindo funcionalidades como precificação, receitas, controle de estoque e outras ferramentas.', 'src': '/images/projects/wire-cotton.avif', 'finished': false }
-} 
+    '1': { 'title': '_wire-cotton', 'description': 'Painel para gerenciar produtos no e-commerce Wire-Cotton.', 'src': '/images/projects/wire-cotton.svg', 'finished': true },
+    '2': { 'title': '_wire-cotton-api', 'description': 'API para integrar dados ao painel do Wire-Cotton.', 'src': '/images/projects/wire-cotton.svg', 'finished': true },
+    '3': { 'title': '_meenu', 'description': 'Gestão de produtos alimentícios com estoque e receitas.', 'src': '/images/projects/wire-cotton.svg', 'finished': false },
+    '4': { 'title': '_nps', 'description': 'Sistema de avaliação baseado no Net Promoter Score.', 'src': '/images/projects/nps.png', 'finished': false }
+}
+
 </script>
 <template>
-    <div class="pb-3 w-full overflow-hidden">
-        <div class="h-16 flex items-center pl-5">
+    <div class="grid grid-cols-1 lg:grid-cols-12 lg:flex-1 lg:h-auto lg:w-auto h-full w-full">
+        <div class="h-16 flex items-center pl-5 lg:hidden">
             <h1 class="text-white">_projetos</h1>
         </div>
-        <details class="bg-[#1E2D3D]">
-            <summary class="text-white pl-5 py-2">
-                <span class="ml-2">tecnologias</span>
-            </summary>
-            <div class="bg-[#011627] pl-5">
-                <ul class="text-white space-y-4 pt-4">
-                    <li v-for="tech in technologies" :key="tech" class="items-center flex gap-3 w-full">
-                        <input type="checkbox" :id="tech">
-                        <label :for="tech" class="w-full flex gap-2">
-                            <img :src="`/icons/techs/${tech.toLowerCase()}.svg`" :alt="`imagem de ${tech}`">
-                            <span>{{ tech }}</span>
-                        </label>
-                    </li>
-                </ul>
-            </div>
-        </details>
-        <div class="w-full h-full flex">
-            <div class="max-w-5xl flex lg:flex-row flex-col gap-6 px-6 lg:px-0">
-                <div v-for="(project, index) in projects" :key="index" class="w-full mt-6 flex flex-col gap-y-4">
-                    <span class="text-[#5565E8] font-bold text-lg">Projeto {{ index }}
-                        <span class="text-[#607B96] ml-2 font-normal">/{{ project.title }}</span>
+        <div class="lg:col-span-2 lg:w-full lg:border-x lg:border-x-[#1E2D3D] space-y-1">
+            <details>
+                <summary class="text-white pl-5 py-2 bg-[#1E2D3D]">
+                    <span class="ml-2">tecnologias</span>
+                </summary>
+                <div class="pl-5">
+                    <ul class="text-white space-y-4 pt-4 h-fit">
+                        <li v-for="tech in technologies" :key="tech" class="items-center flex gap-3 w-full">
+                            <input type="checkbox" :id="tech">
+                            <label :for="tech" class="w-full flex gap-2">
+                                <img :src="`/icons/techs/${tech.toLowerCase()}.svg`" :alt="`imagem de ${tech}`">
+                                <span>{{ tech }}</span>
+                            </label>
+                        </li>
+                    </ul>
+                </div>
+            </details>
+        </div>
+        <div class="lg:w-full lg:col-span-10">
+            <div class="lg:grid lg:grid-cols-8 lg:w-full h-10 hidden border-b-2 border-[#1E2D3D]">
+                <div class="text-[#607B96] cursor-pointer flex items-center justify-between px-4 border-[#1E2D3D] border border-y-0"
+                    v-for="(tab, index) in tabs" :key="index" @click.prevent="loadContent(tab)">
+                    <span :class="{ 'text-white font-semibold': tab === selectedTab }">{{ tab }}</span>
+                    <span @click="closeTab(index, $event)">
+                        <svg width="13" height="13" viewBox="0 0 10 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M5.10015 4.20762L8.81265 0.495117L9.87315 1.55562L6.16065 5.26812L9.87315 8.98062L8.81265 10.0411L5.10015 6.32862L1.38765 10.0411L0.327148 8.98062L4.03965 5.26812L0.327148 1.55562L1.38765 0.495117L5.10015 4.20762Z"
+                                fill="#607B96" />
+                        </svg>
                     </span>
-                    <div class="border-[#1E2D3D] border rounded-2xl">
-                        <div class="w-full h-48 overflow-hidden">
-                            <img :src="project.src" alt="" class="rounded-t-2xl">
-                        </div>
-                        <div class="bg-[#011221] rounded-b-2xl border-t border-[#1E2D3D] p-8 flex flex-col gap-y-6">
-                            <p class="text-[#607B96]">
+                </div>
+            </div>
+            <div class="flex gap-12 flex-wrap items-center justify-center pt-6">
+                <div v-for="(project, index) in projects" :key="index" class="lg:h-72 w-80">
+                    <div class="flex items-center gap-3 pb-2">
+                        <svg width="16" height="13" viewBox="0 0 16 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M15.0802 3.61111V12.2778C15.0802 12.4693 15.0041 12.653 14.8687 12.7885C14.7332 12.9239 14.5495 13 14.358 13H1.35796C1.16642 13 0.982719 12.9239 0.847276 12.7885C0.711833 12.653 0.635742 12.4693 0.635742 12.2778V2.88889H14.358C14.5495 2.88889 14.7332 2.96498 14.8687 3.10042C15.0041 3.23587 15.0802 3.41957 15.0802 3.61111ZM8.15696 1.44444H0.635742V0.722222C0.635742 0.530677 0.711833 0.346977 0.847276 0.211534C0.982719 0.076091 1.16642 0 1.35796 0H6.71252L8.15696 1.44444Z"
+                                stroke="#FFFFFF" />
+                        </svg>
+                        <span class="">{{ project.title }}</span>
+                    </div>
+                    <div
+                        class="bg-[#12171F] rounded-lg border-[1px] border-[#1f2836] text-[#D8DEE9] font-mono flex flex-col gap-4 h-full">
+                        <div class="rounded-2xl flex flex-col gap-4">
+                            <div class="h-36 w-full overflow-hidden">
+                                <img :src="project.src" alt="" class="object-cover rounded h-full w-full">
+                            </div>
+                            <div class="px-6 py-2">
                                 {{ project.description }}
-                            </p>
-                            <a class="flex items-center justify-center bg-[#1C2B3A] rounded-lg text-white w-full h-10">
-                                {{ project.finished ? 'Ver projeto' : '🚧 Em construção' }}
-                            </a>
+                            </div>
                         </div>
                     </div>
                 </div>
