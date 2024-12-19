1a5cb0efa07dddf363da5a5d92f57a261cc908f3
MeenaMisae <meenahiwatashi@gmail.com>
2024-12-18 22:06:21 -0300

    feat: adiciona mensagens de feedback ao enviar formulario

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,12 +1,43 @@
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const form = ref({
+    name: '',
+    email: '',
+    message: ''
+})
+const showSuccessMessage = ref(false)
+const showErrorMessage = ref(false)
+function handleSubmit(event) {
+    const formEl = event.target
+    const formData = new FormData(formEl)
+    axios.post('/', new URLSearchParams(formData), {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    })
+        .then((res) => {
+            form.value = { name: '', email: '', message: '' };
+            showSuccessMessage.value = true;
+            console.log(res);
+        })
+        .catch((error) => {
+            showErrorMessage.value = true;
+            console.log(formData)
+            console.error(error);
+        });
+}
+</script>
 <template>
     <div class="w-full flex items-center py-6 lg:py-0">
         <div class="flex flex-col w-full items-center justify-center gap-3">
-            <h1 class="text-3xl">Me contate!</h1>
-            <div class="max-w-[600px] text-center leading-7">
-                <p>
-                    Quer conversar sobre projetos, colaborações ou tirar dúvidas?
-                </p>
-                <p>Entre em contato comigo.</p>
+            <div class="flex items-center flex-col gap-3">
+                <h1 class="text-3xl">Me contate!</h1>
+                <div class="max-w-[600px] text-center leading-7">
+                    <p>
+                        Quer conversar sobre projetos, colaborações ou tirar dúvidas?
+                    </p>
+                    <p>Entre em contato comigo.</p>
+                </div>
             </div>
             <div>
                 <a href="mailto:contato@meena.com.br" class="btn btn-outline">
@@ -20,12 +51,19 @@
                     </span>
                 </a>
             </div>
-            <form class="w-[330px] lg:w-[600px] flex flex-col gap-3 h-full" name="contact" method="POST"
-                data-netlify="true">
-                <input type="text" name="name" id="name" class="input input-primary" placeholder="nome" required>
-                <input type="email" name="email" id="email" class="input input-primary" placeholder="email" required>
-                <textarea name="message" id="message" placeholder="mensagem" class="textarea textarea-primary"
-                    required></textarea>
+            <form class="w-[330px] lg:w-[600px] flex flex-col gap-3 h-full" name="contact" data-netlify="true"
+                @submit.prevent="handleSubmit">
+                <input v-model="form.name" type="text" name="name" id="name" class="input input-primary"
+                    placeholder="nome" required>
+                <input v-model="form.email" type="text" name="email" id="email" class="input input-primary"
+                    placeholder="email" required>
+                <textarea v-model="form.message" name="message" id="message" placeholder="mensagem"
+                    class="textarea textarea-primary" required></textarea>
+                <input type="hidden" name="form-name" value="contact" />
+                <span class="text-red-600" v-show="showErrorMessage">Ocorreu um erro ao enviar o formulário, tente
+                    novamente mais tarde.</span>
+                <span class="text-green-600" v-show="showSuccessMessage">Obrigada! Responderei o mais rápido
+                    possível :]</span>
                 <button type="submit" class="btn btn-primary h-14">enviar</button>
             </form>
         </div>
